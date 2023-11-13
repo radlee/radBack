407c7c743aa6beff6d5ca848374efcad4dfaa7e8
radlee <prof.lolito@gmail.com>
2023-11-14 00:34:17 +0200

    first

---
+++ models/User.js
@@ -0,0 +1,13 @@
+// models/User.js
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, min: 4, unique: true },
+    password: { type: String, required: true },
+});
+
+// Use the same name 'User' instead of 'UserModel'
+const User = model('User', UserSchema);
+
+module.exports = User;
