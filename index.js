22d82ecca24f5c876af9f77b8f202e4ac08e1336
lolito <prof.lolito@gmail.com>
2023-11-14 11:33:09 +0200

    Added a link for radFront

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret =  'ujk857y383ifnkmlertert6357';
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'https://radfront.onrender.com'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
